3bed2fe09ec40cb05c05786587d8501602d56d3a
Dariya <dariya_prok@ukr.net>
2018-11-22 23:10:49 +0200

    commit

---
+++ TTRestaurantBooking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TTRestaurantBooking
+//
+//  Created by Dariya on 11/22/18.
+//  Copyright © 2018 dariyaprokopovych. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
